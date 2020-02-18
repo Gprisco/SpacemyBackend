f0610e7d2f17b6d7a02344cbb29939bc4c958583
Giovanni Prisco <gprisco01@gmail.com>
2020-02-18 15:11:31 +0100

    Added heroku postgres config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,31 +5,39 @@ import Vapor
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentPostgreSQLProvider())
-
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
     
-//    Setup Database Configuration
+    //    Setup Database Configuration
     
-    let databaseConfig = PostgreSQLDatabaseConfig (hostname: "localhost", port: 5432, username: "academyguy", database: "spacemy")
-
-    // Configure a SQLite database
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)! } else {
+        databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: "localhost",
+            port: 5432,
+            username: "academyguy",
+            database: "spacemy"
+        )
+    }
+    
     let postgre = PostgreSQLDatabase(config: databaseConfig)
     
-//    Register the configured database
+    //    Register the configured database
     var databases = DatabasesConfig()
     databases.add(database: postgre, as: .psql)
     services.register(databases)
-//
-//    // Configure migrations
+    //
+    //    // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Academy_Guys.self, database: .psql)
     migrations.add(model: Category.self, database: .psql)
@@ -39,6 +47,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: User.self, database: .psql)
     migrations.add(model: UserEventPivot.self, database: .psql)
     migrations.add(model: CollabCategoryPivot.self, database: .psql)
-//    migrations.add(model: Todo.self, database: .sqlite)
+    //    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 }
